d1ced7687e3a670472d39ea294b17333f56a5de3
Naveen <naveendevinda163@gmail.com>
2020-01-23 16:56:15 +0530

    add images package

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text("Naveen"),
+  
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     child: Text("Naveen"),
+  //   );
+  // }
+  @override            
+  Widget build(BuildContext context) {            
+    return MaterialApp(
+      //title: 'Welcome to Flutter',
+      //title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Image.asset('assets/home.png', fit: BoxFit.cover)
+          //title: Text('First App'),
+          //title: Text('Flutter layout demo'),
+        ),
+        body: Center(
+          // child: Column(
+          //   children: <Widget>[
+          //     Text("Naveen"),
+          //     RaisedButton(
+          //       onPressed: () {},
+          //       child: Text('Click me'),
+          //       color: Colors.lightBlue,
+          //     )
+          //   ],
+          // ),
+          child: RaisedButton(
+            onPressed: () {},
+            child: Text("Click Me"),
+            color: Colors.lightBlue,
+          ),
+        ),
+      ),
     );
   }
 }
